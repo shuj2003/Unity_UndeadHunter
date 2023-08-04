8b73f2cc01e08d91ef74751cf4a1ce9b672cbc1e
JyoJyo <shuj2003@gmail.com>
2023-08-04 18:55:23 +0900

    joystickの追加

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
 
+    [SerializeField] VariableJoystick variableJoystick;
     public Vector2 inputVec;
     public float speed;
 
@@ -15,16 +16,18 @@ public class Player : MonoBehaviour
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        speed = 3;
     }
 
     // Update is called once per frame
-    //void Update()
-    //{
+    void Update()
+    {
+        inputVec = variableJoystick.Direction;
 
-    //    inputVec.x = Input.GetAxisRaw("Horizontal");
-    //    inputVec.y = Input.GetAxisRaw("Vertical");
+        //inputVec.x = Input.GetAxisRaw("Horizontal");
+        //inputVec.y = Input.GetAxisRaw("Vertical");
 
-    //}
+    }
 
     void FixedUpdate()
     {
@@ -37,29 +40,9 @@ public class Player : MonoBehaviour
         //移動距離を設定
         //rigid.MovePosition(rigid.position + inputVec);
 
-        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
 
-    }
-
-    void OnMove(InputValue value)
-    {
-
-        inputVec = value.Get<Vector2>();
-
-    }
-
-    // PlayerInputから通知されるコールバック
-    public void OnNavigate(InputAction.CallbackContext context)
-    {
-        // performedコールバックだけをチェックする
-        if (!context.performed) return;
-
-        // スティックの2軸入力取得
-        var inputValue = context.ReadValue<Vector2>();
-
-        // 入力値をログ出力
-        Debug.Log($"OnNavigate : value = {inputValue}");
     }
 
 }
