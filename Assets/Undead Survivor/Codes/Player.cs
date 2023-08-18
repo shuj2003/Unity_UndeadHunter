45b2fc4c491776be41d82a2386ea04f54c2ae06d
jyo <jyo@alala.co.jp>
2023-08-18 18:37:21 +0900

    無限マップ カメラ追跡

--- Assets/Undead Survivor/Codes/Player.cs
+++ Assets/Undead Survivor/Codes/Player.cs
@@ -11,15 +11,19 @@ public class Player : MonoBehaviour
     public float speed;
 
     private Rigidbody2D rigid;
+    private SpriteRenderer sprite;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
         speed = 3;
     }
 
-    // Update is called once per frame
+    // 毎フレーム呼ばれる基本処理を書くところ
     void Update()
     {
         inputVec = variableJoystick.Direction;
@@ -29,6 +33,20 @@ public class Player : MonoBehaviour
 
     }
 
+    // 様々な計算が終わった後の最終調整を書くところ。
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            sprite.flipX = inputVec.x < 0;
+        }
+
+    }
+
+    //独自のスレートで行います。物理演算に関係する処理を書くところ（ゲーム内時間に関係するもの）。
+    //デフォルトは0.02秒単位で呼ばれます。時間設定できます。
     void FixedUpdate()
     {
         //力を加える
---
+++ Assets/Undead Survivor/Codes/RePosition.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RePosition : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Area"))
+            return;
+
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 myPos = transform.position;
+        float diffx = Mathf.Abs(playerPos.x - myPos.x);
+        float diffy = Mathf.Abs(playerPos.y - myPos.y);
+
+        Vector3 playerDir = GameManager.instance.player.inputVec;
+        float dirX = playerDir.x < 0 ? -1 : 1;
+        float dirY = playerDir.y < 0 ? -1 : 1;
+
+        switch (transform.tag)
+        {
+            case "Ground":
+                if(diffx > diffy)
+                {
+                    transform.Translate(Vector3.right * dirX * 40);
+                }
+                else if (diffx < diffy)
+                {
+                    transform.Translate(Vector3.up * dirY * 40);
+                }
+                break;
+
+            case "Enemy":
+
+                break;
+        }
+
+    }
+}
